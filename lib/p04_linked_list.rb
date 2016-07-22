9b9cf113e3f6a29e027a3241262e8bedf5d3a431
App Academy Student <student@appacademy.io>
2016-07-22 15:37:23 -0700

    insert for linked list

--- lib/p04_linked_list.rb
+++ lib/p04_linked_list.rb
@@ -15,7 +15,10 @@ end
 
 class LinkedList
   def initialize
-    
+    @head = Link.new
+    @tail = Link.new
+    @head.next = @tail
+    @tail.prev = @head
   end
 
   def [](i)
@@ -24,12 +27,16 @@ class LinkedList
   end
 
   def first
+    @head.next
   end
 
   def last
+    @tail.prev
+    # @tail
   end
 
   def empty?
+    @head.next == @tail
   end
 
   def get(key)
@@ -39,16 +46,43 @@ class LinkedList
   end
 
   def insert(key, val)
+    new_link = Link.new(key, val)
+
+    second_to_last = @tail.prev
+# debugger
+    @tail.prev = new_link
+    new_link.next = @tail
+    new_link.prev = second_to_last
+    second_to_last.next = new_link
+
+
   end
 
   def remove(key)
   end
 
   def each
+    i = @head.next
+    until i == nil
+      p i.val
+      i = i.next
+    end
   end
 
   # uncomment when you have `each` working and `Enumerable` included
   # def to_s
   #   inject([]) { |acc, link| acc << "[#{link.key}, #{link.val}]" }.join(", ")
   # end
+end
+
+if __FILE__ == $PROGRAM_NAME
+
+  list = LinkedList.new
+
+  { first: 1, second: 2, third: 3 }.each do |key, val|
+    list.insert(key, val)
+  end
+
+  p list.each
+
 end
--- spec/p04_linked_list_spec.rb
+++ spec/p04_linked_list_spec.rb
@@ -1,3 +1,4 @@
+require 'byebug'
 require 'rspec'
 require_relative 'spec_helper'
 require 'p04_linked_list'
@@ -24,8 +25,8 @@ describe LinkedList do
     end
   end
 
-  describe "#insert" do 
-    it "inserts links" do 
+  describe "#insert" do
+    it "inserts links" do
       empty_list.insert(:first, 1)
       expect(empty_list.empty?).to be false
     end
