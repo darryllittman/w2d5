f1128358ffa725d3746342c580342e69934c5dad
App Academy Student <student@appacademy.io>
2016-07-22 11:36:05 -0700

    Complete 01_int_set.

---
+++ lib/p01_int_set.rb
@@ -0,0 +1,110 @@
+class MaxIntSet
+  def initialize(max)
+    @store = Array.new(max, false)
+  end
+
+  def insert(num)
+    validate!(num)
+    @store[num] = true
+  end
+
+  def remove(num)
+    validate!(num)
+    @store[num] = false
+  end
+
+  def include?(num)
+    validate!(num)
+    @store[num] == true
+  end
+
+  private
+
+  def is_valid?(num)
+    num.between?(0, @store.count - 1)
+  end
+
+  def validate!(num)
+    raise "Out of bounds" unless is_valid?(num)
+  end
+end
+
+
+class IntSet
+  def initialize(num_buckets = 20)
+    @store = Array.new(num_buckets) { Array.new }
+  end
+
+  def insert(num)
+    @store[num] = true
+  end
+
+  def remove(num)
+    @store[num] = false
+  end
+
+  def include?(num)
+    @store[num] == true
+  end
+
+  private
+
+  def [](num)
+    # optional but useful; return the bucket corresponding to `num`
+    @store[num]
+  end
+
+  def num_buckets
+    @store.length
+  end
+end
+
+class ResizingIntSet
+  attr_reader :count
+
+  def initialize(num_buckets = 20)
+    @store = Array.new(num_buckets) { Array.new }
+    @count = 0
+  end
+
+  def insert(num)
+    if count == num_buckets
+      resize!
+    end
+
+    i = num % num_buckets
+    @store[i] << num
+
+    @count += 1
+  end
+
+  def remove(num)
+    i = num % num_buckets
+    @store[i].delete(num)
+  end
+
+  def include?(num)
+    @store[num % num_buckets].include?(num)
+  end
+
+  private
+
+  def [](num)
+    # optional but useful; return the bucket corresponding to `num`
+  end
+
+  def num_buckets
+    @store.length
+  end
+
+  def resize!
+    # @store << []
+    @store.size.times { @store << [] }
+    old_store = @store.flatten
+
+    old_store.each do |el|
+      i = el % num_buckets
+      @store[i] << el
+    end
+  end
+end
